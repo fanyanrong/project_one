15eb4f0914ce34ba50586a908bcb142e02b1d9e3
fyr <846456307qq.com>
2019-06-11 17:25:40 +0800

    登录页面完成

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,47 +1,76 @@
-import React, { Component } from 'react'
+import React, { useState,useEffect } from 'react'
 import { connect } from 'dva'
-import styles from './index.css';
+import styles from './index.scss';
 import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
-class LoginPage extends Component {
-    componentDidMount() {
-        //调登录接口
-        let { login } = this.props
+function LoginPage(props){
+    console.log('props...',props)
+    let {login}=props
+    useEffect(()=>{
         login({
-            user_name: 'chenmanjie',
-            user_pwd: 'Chenmanjie123!'
-        });
-    }
+            user_name:'chenmanjie',
+            user_pwd:'Chenmanjie123!'
+        })
+    },[]);
 
-    render() {
-        return (
-            <div className={styles.index}>
-                <div className={styles.login}>
-                    <Form className="login-form" className={styles.login_form}>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                placeholder="请输入用户名"
-                            />
-                        </Form.Item>
-                        <Form.Item>
-                            <Input
-                                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                type="password"
-                                placeholder="请输入用户密码"
-                            />
-                        </Form.Item>
-                        <Form.Item className={styles.form_pwd}>
-                            <Checkbox>记住密码</Checkbox>
-                            <a className="login-form-forgot" href="">忘记密码</a><br />
-                        </Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button" className={styles.login_form_button}>登录</Button>
-                    </Form>
-                </div>
-            </div>
-        )
-    }
+    //处理表单提交
+    let handleSubmit = e => {
+        e.preventDefault();
+            props.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      };
+    //表单校验
+    const { getFieldDecorator } = props.form;
+    return (
+        <div className="login">
+        <Form onSubmit={handleSubmit} className="login-form">
+        <Form.Item>
+          {getFieldDecorator('username', {
+            rules: [{ required: true, message: '请输入你的用户名!' }],
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="请输入用户名"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: '请输入你的密码!' },
+            { pattern: /^(?![0-9]+$)(?![a-z]+$)(?![A-Z]+$)(?!([^(0-9a-zA-Z)])+$)^.{8,16}$/, message: '密码校验失败!密码包含大小写字母、数字、特殊符号!' }],
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="请输入用户密码"
+            />,
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(<Checkbox>记住密码</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            忘记密码
+          </a>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            登录
+          </Button>
+        </Form.Item>
+      </Form>
+      </div>
+    )
 }
+
+//props的类型检查
+LoginPage.propTypes={}
+//props的默认值
+LoginPage.defaultProps={}
+
 
 const mapStateToProps = state => {
     return {}
@@ -58,4 +87,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(LoginPage)
+export default connect(mapStateToProps, mapDispatchToProps)(Form.create()(LoginPage))
